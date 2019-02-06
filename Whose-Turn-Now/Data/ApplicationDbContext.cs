b3d42dec69adeebf2bb529f19a3a4c0eb33b0f83
Brian D. Conversi <bdconversi@yahoo.com>
2019-02-05 19:41:13 -0600

    Add project files.

---
+++ Whose-Turn-Now/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Whose_Turn_Now.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Whose-Turn-Now/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Whose_Turn_Now.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
