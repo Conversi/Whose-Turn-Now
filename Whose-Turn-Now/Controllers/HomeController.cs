8851624db869a309038e9e7683591430379a6128
Brian D. Conversi <bdconversi@yahoo.com>
2019-02-05 20:18:29 -0600

    initial commit

--- Whose-Turn-Now/Controllers/HomeController.cs
+++ Whose-Turn-Now/Controllers/HomeController.cs
@@ -17,14 +17,14 @@ namespace Whose_Turn_Now.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "How To Create Shared Tasks";
 
             return View();
         }
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = "Feel free to reach out";
 
             return View();
         }
